9d3011d8f125c2e42b94952d05229be0cc72baff
Mohammadreza Amini <mohammadreza99a@yahoo.com>
2020-07-10 00:57:58 +0200

    Updates

---
+++ precache-manifest.03fbbf95a514ba811fc692a12bdd1482.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a78ed6d887659460a63f4cbcf329ef57",
+    "url": "/music-info-finder/index.html"
+  },
+  {
+    "revision": "160e4f7bb1355896a701",
+    "url": "/music-info-finder/static/css/main.0ba1cb4d.chunk.css"
+  },
+  {
+    "revision": "209785541f44ec8e4e09",
+    "url": "/music-info-finder/static/js/2.856a8989.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/music-info-finder/static/js/2.856a8989.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "160e4f7bb1355896a701",
+    "url": "/music-info-finder/static/js/main.733af4c7.chunk.js"
+  },
+  {
+    "revision": "8b1207f8324bcb814cf8",
+    "url": "/music-info-finder/static/js/runtime-main.65ba6c9e.js"
+  }
+]);
