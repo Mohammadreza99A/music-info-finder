803e422dd1017a9d9eb75f8905d0c98b8d392e46
Mohammadreza Amini <mohammadreza99a@yahoo.com>
2020-07-11 18:17:56 +0200

    added dark mode theme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -11,19 +11,40 @@ import TrackInfo from './components/tracks/TrackInfo';
 import { ContextController } from './context';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(getInitilTheme());
+
+  function changeTheme(mode) {
+    setDarkMode(mode);
+  }
+
+  useEffect(() => {
+    localStorage.setItem('dark', JSON.stringify(darkMode));
+  }, [darkMode]);
+
+  function getInitilTheme() {
+    const savedTheme = JSON.parse(localStorage.getItem('dark'));
+    // const userPreferanceThem = getPreferedTheme();
+    return savedTheme || false;
+  }
+
+  // function getPreferedTheme() {
+  //   if (!window.matchMedia) return;
+  //   return window.matchMedia('(prefers-color-scheme: dark)');
+  // }
+
   return (
     <ContextController>
       <Router basename="/">
-        <React.Fragment>
-          <Navbar />
-          <div className="container">
+        <div className={darkMode ? 'dark-mode' : 'light-mode'}>
+          <Navbar themeMode={darkMode} onChange={changeTheme} />
+          <div>
             <Switch>
               <Route exact path="/" component={Index}></Route>
               <Route exact path="/info/track/:id" component={TrackInfo}></Route>
             </Switch>
           </div>
           <Footer />
-        </React.Fragment>
+        </div>
       </Router>
     </ContextController>
   );
--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
-function Navbar() {
+function Navbar({ darkMode, onChange }) {
+  function changeTheme(mode) {
+    onChange((prevMode) => !prevMode);
+  }
+
   return (
     <nav className="navbar sticky-top navbar-dark bg-dark mb-5">
       <span className="navbar-brand mb-0 h1 mx-auto">Music Info Finder</span>
+      <div className="toggle-box mx-auto">
+        <input
+          type="checkbox"
+          onChange={() => changeTheme(darkMode)}
+          id="toggle-box-checkbox"
+        />
+        <label
+          htmlFor="toggle-box-checkbox"
+          className="toggle-box-label-left"
+        ></label>
+        <label
+          htmlFor="toggle-box-checkbox"
+          className="toggle-box-label"
+        ></label>
+      </div>
     </nav>
   );
 }
